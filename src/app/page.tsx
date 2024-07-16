c76a37d156fe9e73cb238f58af303aa65c3ccc39
SoundRabbit <soundrabbitstd@gmail.com>
2024-07-16 16:36:11 +0900

    feat: page.tsxのスタイリング

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import { PopulationChartContainer } from '@/components/molecules/PopulationChartContainer';
 
+import styles from './page.module.scss';
+
 const App = () => {
   return (
-    <main>
-      <h1>コーディングテスト | 株式会社ゆめみ</h1>
-      <PopulationChartContainer />
-    </main>
+    <div className={styles['page-container']}>
+      <div className={styles['page-content']}>
+        <header className={styles['header']}>
+          <h1 className={styles['header-heading']}>コーディングテスト | 株式会社ゆめみ</h1>
+        </header>
+        <main className={styles['main']}>
+          <PopulationChartContainer />
+        </main>
+      </div>
+    </div>
   );
 };
 
