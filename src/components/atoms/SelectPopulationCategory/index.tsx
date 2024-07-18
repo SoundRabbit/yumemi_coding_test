967bf1dce7b4ffc80dffc015eb90f016d3a3c87b
SoundRabbit <soundrabbitstd@gmail.com>
2024-07-18 18:16:26 +0900

    feat: SelectPopulationCategoryコンポーネントをスタイリング

--- src/components/atoms/SelectPopulationCategory/index.tsx
+++ src/components/atoms/SelectPopulationCategory/index.tsx
@@ -1,5 +1,7 @@
 import { useCallback } from 'react';
 
+import styles from './index.module.scss';
+
 export type SelectPopulationCategoryProps = {
   categoryLabel: string;
   onChange: (categoryLabel: string) => void;
@@ -14,11 +16,26 @@ export const SelectPopulationCategory = ({ categoryLabel, onChange }: SelectPopu
   );
 
   return (
-    <select value={categoryLabel} onChange={handleChange}>
-      <option value='総人口'>総人口</option>
-      <option value='年少人口'>年少人口</option>
-      <option value='生産年齢人口'>生産年齢人口</option>
-      <option value='老年人口'>老年人口</option>
-    </select>
+    <div className={styles['container']}>
+      <select value={categoryLabel} onChange={handleChange} className={styles['select']}>
+        <option value='総人口' data-checked={categoryLabel === '総人口'} className={styles['select-option']}>
+          総人口
+        </option>
+        <option value='年少人口' data-checked={categoryLabel === '年少人口'} className={styles['select-option']}>
+          年少人口
+        </option>
+        <option
+          value='生産年齢人口'
+          data-checked={categoryLabel === '生産年齢人口'}
+          className={styles['select-option']}
+        >
+          生産年齢人口
+        </option>
+        <option value='老年人口' data-checked={categoryLabel === '老年人口'} className={styles['select-option']}>
+          老年人口
+        </option>
+      </select>
+      <div className={styles['arrow']}>&#x25BC;</div>
+    </div>
   );
 };
