111f1b6cfb779bcf3fe2215334d4d160a8188a3f
SoundRabbit <soundrabbitstd@gmail.com>
2024-07-19 18:19:19 +0900

    refactor: optionのdata-checkedをdata-selectedの修正

--- src/components/atoms/SelectPopulationCategory/index.tsx
+++ src/components/atoms/SelectPopulationCategory/index.tsx
@@ -23,20 +23,20 @@ export const SelectPopulationCategory = ({ categoryLabel, onChange }: SelectPopu
   return (
     <div className={styles['container']}>
       <select value={categoryLabel} onChange={handleChange} className={styles['select']}>
-        <option value='総人口' data-checked={categoryLabel === '総人口'} className={styles['select-option']}>
+        <option value='総人口' data-selected={categoryLabel === '総人口'} className={styles['select-option']}>
           総人口
         </option>
-        <option value='年少人口' data-checked={categoryLabel === '年少人口'} className={styles['select-option']}>
+        <option value='年少人口' data-selected={categoryLabel === '年少人口'} className={styles['select-option']}>
           年少人口
         </option>
         <option
           value='生産年齢人口'
-          data-checked={categoryLabel === '生産年齢人口'}
+          data-selected={categoryLabel === '生産年齢人口'}
           className={styles['select-option']}
         >
           生産年齢人口
         </option>
-        <option value='老年人口' data-checked={categoryLabel === '老年人口'} className={styles['select-option']}>
+        <option value='老年人口' data-selected={categoryLabel === '老年人口'} className={styles['select-option']}>
           老年人口
         </option>
       </select>
